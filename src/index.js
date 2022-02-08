40dd2c518912ca4499c7d599416b107a1fc5c99e
Rafli <rahmatrafli1@gmail.com>
2022-02-08 10:19:19 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Header extends React.Component {
+  constructor() {
+    super();
+    // membuat object state
+    this.state = {
+      title: "React JS",
+      subTitle: "Belajar React JS",
+    };
+  }
+
+  changeTitle = () => {
+    this.setState({
+      title: "New Title",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.title}</h1>
+        <h2>{this.state.subTitle}</h2>
+        <button onClick={this.changeTitle}>Ubah Judul</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Header />, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
